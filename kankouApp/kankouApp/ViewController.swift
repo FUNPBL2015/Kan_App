05733cef999acda569f2b810b6520503196e8cde
takuyamakawa <b1013167@fun.ac.jp>
2015-06-18 20:23:56 +0900

    各画面にナビゲーションバーを配置

--- kankouApp/kankouApp/ViewController.swift
+++ kankouApp/kankouApp/ViewController.swift
@@ -22,13 +22,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.navigationController?.hidesBarsOnTap = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
