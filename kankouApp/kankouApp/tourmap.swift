983fffeb82ef37bc7d7e69a7d068d448dbdbc9ae
Kenta Iwami <b1013068@fun.ac.jp>
2015-06-30 12:38:39 +0900

    ストーリーボードTourmap内にあるボタンの関連付けを削除

--- kankouApp/kankouApp/tourmap.swift
+++ kankouApp/kankouApp/tourmap.swift
@@ -10,9 +10,7 @@ import UIKit
 
 class tourmap: UIViewController {
 
-    @IBOutlet weak var gourmetBtn: UIButton! //グルメボタン
-    @IBOutlet weak var tourBtn: UIButton! //観光ボタン
-    @IBOutlet weak var souvenirBtn: UIButton! //お土産ボタン
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,16 +23,13 @@ class tourmap: UIViewController {
     }
     
     //グルメをタップした時の動作
-    @IBAction func tapGourmet(sender: AnyObject) {
-    }
+
     
     //観光をタップした時の動作
-    @IBAction func tapTour(sender: AnyObject) {
-    }
+
     
     //お土産をタップした時の動作
-    @IBAction func tapSouvenir(sender: AnyObject) {
-    }
+
     
     /*
     // MARK: - Navigation
