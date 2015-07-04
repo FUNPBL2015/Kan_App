03e9dde510261aab378a286ba125bca41b1d9bac
Kenta Iwami <b1013068@fun.ac.jp>
2015-07-04 18:20:31 +0900

    Detailページに部品を設置

--- kankouApp/kankouApp/Detail.swift
+++ kankouApp/kankouApp/Detail.swift
@@ -7,13 +7,29 @@
 //
 
 import UIKit
+import MapKit
 
-class Detail: UIViewController {
+class Detail: UIViewController,MKMapViewDelegate{
 
+    @IBOutlet weak var StoreName: UILabel!
+    @IBOutlet weak var StoreMap: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        StoreName.text = "CEK倶楽部"
+        StoreName.font = UIFont.systemFontOfSize(25)
+        StoreName.textAlignment = NSTextAlignment.Center
+        
+        //Map関連
+        self.StoreMap?.delegate = self
+        
+        var region: MKCoordinateRegion = self.StoreMap!.region
+        var location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(41.677589, 140.433941)
+        region.center = location
+        region.span.latitudeDelta = 0.005
+        region.span.longitudeDelta = 0.005
+        
+        self.StoreMap!.setRegion(region, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
