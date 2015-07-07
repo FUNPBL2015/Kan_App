2ba1e66bf1b55cb5af1056f2890d9927ee235c56
Toshiki Ikda <b1013001@fun.ac.jp>
2015-07-07 20:10:56 +0900

    選択画面

--- kankouApp/kankouApp/eating.swift
+++ kankouApp/kankouApp/eating.swift
@@ -10,10 +10,20 @@ import UIKit
 
 class eating: UIViewController {
 
+    @IBOutlet weak var pic1: UIImageView!
+    @IBOutlet weak var pic2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let myImage1: UIImage = UIImage(named: "aburidon-aoki001.jpg")!
+        // 画面に画像を設定する
+        pic1.image = myImage1
+        
+        let myImage2: UIImage = UIImage(named: "tokusan-gurume005.jpg")!
+        // 画面に画像を設定する
+        pic2.image = myImage2
     }
 
     override func didReceiveMemoryWarning() {
